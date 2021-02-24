c6de2a08a3e30834af95a2f327f4827941f81369
Kirk Lennon <kirklennon@me.com>
2021-02-23 21:09:14 -0800

    Enable support for multiple patents and assignee field

--- puller/forms.py
+++ puller/forms.py
@@ -3,19 +3,27 @@ from django.core.exceptions import ValidationError
 import re
 
 class PatentForm(forms.Form):
-	patents = forms.CharField(max_length = 100, help_text='Enter a patent number')
+	patents = forms.CharField(widget=forms.Textarea(attrs={"rows":10, "cols":20}), label='Patent Numbers', help_text='Enter a patent number')
 	
 	def clean_patents(self):
-		data = self.cleaned_data['patents']
-
-		# Check if a patent number is valid
-		# Allows the following formats, in caps and lowercase:
-		# 9,123,456 RE1,1234 D999,123 (w/ or w/o commas)
-		# Plus prepending US or appending A1/A2/B1/B2
-		if  bool(re.match('^[USusREreDd,0-9AaBb]+$', data)) is False:
-			raise ValidationError('Invalid patent number')
-
-		# Check if patent has the right amount of characters
-			# implement later
-
-		return data
+		rawPatentInput = self.cleaned_data['patents']
+		patentNumberList = []
+		
+		for rawPatentNumber in rawPatentInput.splitlines():
+			rawPatentNumber = rawPatentNumber.strip() 
+			# Check if a patent number is valid
+			# Allows the following formats, in caps and lowercase:
+			# 9,123,456 RE1,1234 D999,123 (w/ or w/o commas)
+			# Plus prepending US or appending A1/A2/B1/B2/E/S
+			if  bool(re.match('^[USusREreDd,0-9AaBbEeSs]+$', rawPatentNumber)) is False:
+				raise ValidationError('Invalid patent number')
+	
+			# Check if patent has the right amount of characters
+				# implement later
+			
+			# craft a regex	replacement instead at some point
+			barePatentNumber = rawPatentNumber.replace(',', '').replace('US', '').replace('us', '').replace('A1', '').replace('a1', '').replace('A2', '').replace('a2', '').replace('B1', '').replace('b1', '').replace('B2', '').replace('b2', '').replace('E', '').replace('e', '').replace('E1', '').replace('e1', '').replace('S', '').replace('s', '').replace('S1', '').replace('s1', '')
+		
+			patentNumberList.append(barePatentNumber)
+		# new list
+		return patentNumberList
--- puller/views.py
+++ puller/views.py
@@ -44,7 +44,6 @@ def form_view(request_iter):
 
 	if request_iter.method == "POST":
 		value = Patentform(request_iter.POST)
-		print("Patents: ",value)
 	return  render(request_iter,'form_handling.html', {"form": form})
 	
 
@@ -58,32 +57,39 @@ def patent_form(request):
 
 		# Check if the form is valid:
 		if form.is_valid():
-			# process the data in form.cleaned_data as required (here we just write it to the model due_back field)
-			cleanpatent = form.cleaned_data['patents']
 			
-			pn = cleanpatent.replace(',', '')
-			url = 'http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO1&Sect2=HITOFF&d=PALL&p=1&u=%2Fnetahtml%2FPTO%2Fsrchnum.htm&r=1&f=G&l=50&s1={}.PN.&OS=PN/{}&RS=PN/{}'.format(pn,pn,pn)
+			patentDictList = []
+			
+			patentNumberList = form.cleaned_data['patents']
+			for pn in patentNumberList:
+				
+				patentDict = {'number' : pn}
+				
+				# scrape everything but assignee
+				url = 'http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO1&Sect2=HITOFF&d=PALL&p=1&u=%2Fnetahtml%2FPTO%2Fsrchnum.htm&r=1&f=G&l=50&s1={}.PN.&OS=PN/{}&RS=PN/{}'.format(pn,pn,pn)
+	
+				request_header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
+				page = requests.get(url, headers=request_header).text
+	
+				soup = BeautifulSoup(page, 'html.parser')
+				
+				patentDict['scrapedTitle'] = soup.find("font", {"size": "+1"}).text
+				
+				inventorcolumn = soup.find("th").parent.td
+				inventorsraw = inventorcolumn.find_all('b')
+				patentDict['inventors'] = namelister(inventorsraw)
+				patentDict['abstract'] = soup.find('p').text
 
-			request_header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
-			page = requests.get(url, headers=request_header).text
-
-			soup = BeautifulSoup(page, 'html.parser')
-			
-			scrapedtitle = soup.find("font", {"size": "+1"}).text
-			
-			inventorcolumn = soup.find("th").parent.td
-			inventorsraw = inventorcolumn.find_all('b')
-			abstract = soup.find('p').text
-			inventors = namelister(inventorsraw)
-			
-			print('Inventors: ', namelister(inventorsraw))
-			# send patent number:
-			# return HttpResponse(cleanpatent, content_type='text/plain')
+				
+				# pull assignment information
+				url = 'https://assignment-api.uspto.gov/patent/lookup?query={}&filter=PatentNumber&fields=main'.format(pn)
+				tree = ET.fromstring(requests.get(url).text)
+				patentDict['assignee'] = tree.findtext(".//*[@name='patAssigneeName']/str") 
+				
+				patentDictList.append(patentDict)
+				
 			context = {
-				'patent_number': cleanpatent,
-				'patent_title': scrapedtitle,
-				'patent_abstract': abstract,
-				'patent_inventors': inventors,
+				'patentDictList' : patentDictList,
 			}
 			return render(request, 'puller/patent_results.html', context)
 
