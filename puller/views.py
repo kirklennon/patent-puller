a4c9fc586abb13dd5dd4797c8bbdb1255b31ee72
Kirk Lennon <kirklennon@me.com>
2021-02-24 20:08:15 -0800

    Use ThreadPoolExecutor to pull patents simultaneously

--- puller/views.py
+++ puller/views.py
@@ -4,10 +4,10 @@ from django.urls import reverse
 
 from puller.forms import PatentForm
 
-
 import requests
 from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
+import concurrent.futures
 
 def nameflipper(raw):
 	'''Takes name in the format 'Doe; John A.'   
@@ -47,6 +47,28 @@ def form_view(request_iter):
 	return  render(request_iter,'form_handling.html', {"form": form})
 	
 
+def puller(pn):
+	patentDict = {'number' : pn}
+	# scrape everything but assignee
+	url = 'http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO1&Sect2=HITOFF&d=PALL&p=1&u=%2Fnetahtml%2FPTO%2Fsrchnum.htm&r=1&f=G&l=50&s1={}.PN.&OS=PN/{}&RS=PN/{}'.format(pn,pn,pn)
+
+	request_header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
+	page = requests.get(url, headers=request_header).text
+
+	soup = BeautifulSoup(page, 'html.parser')
+	
+	patentDict['title'] = soup.find("font", {"size": "+1"}).text
+	
+	inventorcolumn = soup.find("th").parent.td
+	inventorsraw = inventorcolumn.find_all('b')
+	patentDict['inventors'] = namelister(inventorsraw)
+	patentDict['abstract'] = soup.find('p').text
+
+	# pull assignment information
+	url = 'https://assignment-api.uspto.gov/patent/lookup?query={}&filter=PatentNumber&fields=main'.format(pn)
+	tree = ET.fromstring(requests.get(url).text)
+	patentDict['assignee'] = tree.findtext(".//*[@name='patAssigneeName']/str") 
+	return patentDict
 def patent_form(request):
 
 	# If this is a POST request then process the Form data
@@ -61,36 +83,16 @@ def patent_form(request):
 			patentDictList = []
 			
 			patentNumberList = form.cleaned_data['patents']
-			for pn in patentNumberList:
-				
-				patentDict = {'number' : pn}
-				
-				# scrape everything but assignee
-				url = 'http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO1&Sect2=HITOFF&d=PALL&p=1&u=%2Fnetahtml%2FPTO%2Fsrchnum.htm&r=1&f=G&l=50&s1={}.PN.&OS=PN/{}&RS=PN/{}'.format(pn,pn,pn)
-	
-				request_header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
-				page = requests.get(url, headers=request_header).text
-	
-				soup = BeautifulSoup(page, 'html.parser')
-				
-				patentDict['title'] = soup.find("font", {"size": "+1"}).text
-				
-				inventorcolumn = soup.find("th").parent.td
-				inventorsraw = inventorcolumn.find_all('b')
-				patentDict['inventors'] = namelister(inventorsraw)
-				patentDict['abstract'] = soup.find('p').text
+			
+			with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+				future_to_patent = {executor.submit(puller, pn): pn for pn in patentNumberList}
+				for future in concurrent.futures.as_completed(future_to_patent):
+					patentDictList.append(future.result())
 
-				
-				# pull assignment information
-				url = 'https://assignment-api.uspto.gov/patent/lookup?query={}&filter=PatentNumber&fields=main'.format(pn)
-				tree = ET.fromstring(requests.get(url).text)
-				patentDict['assignee'] = tree.findtext(".//*[@name='patAssigneeName']/str") 
-				
-				patentDictList.append(patentDict)
-				
 			context = {
 				'patentDictList' : patentDictList,
 			}
+			
 			return render(request, 'puller/patent_results.html', context)
 
 	# If this is a GET (or any other method) create the default form.
