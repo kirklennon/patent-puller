afd4c127f38b326ac7c121bc9d21a6047a049453
Kirk Lennon <kirklennon@me.com>
2022-10-03 20:38:43 -0700

    Update views.py
    The USPTO sunsetted the search results page used to pull the patent information; replaced that with a JSON call documented at https://developer.uspto.gov/ibd-api/swagger-ui/index.html

--- puller/views.py
+++ puller/views.py
@@ -5,35 +5,28 @@ from django.urls import reverse
 from puller.forms import PatentForm
 
 import requests
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
 import concurrent.futures
 
 def nameflipper(raw):
-	'''Takes name in the format 'Doe; John A.'   
-	and returns it as 'John A. Doe'
+	'''Takes name in the format 'Last First M.'   
+	and returns it as 'First M. Last'
 	'''
-	splitname = raw.split(';')
-	return splitname[1].lstrip() + ' ' + splitname[0]
+	splitName = raw.split(' ', 1)
+	return splitName[1] + ' ' + splitName[0]
 	
-def namelister(inventorsraw):
+def namelister(inventorsRaw):
 	'''Combines all inventors into a    
 	single comma-separated list'
 	'''
-	inventorlist = []
-	for inventor in inventorsraw:
-		if inventor.text.startswith(','):
-			inventorlist.append(nameflipper(inventor.text[2:]))
-		elif len(inventor.text) == 2:	
-		#country code for international inventors is bolded; need to skip
-			pass
-		else: 
-			inventorlist.append(nameflipper(inventor.text))
-	
-	if len(inventorlist) == 1:
-		return inventorlist[0]
+	inventorList = []
+	for inventor in inventorsRaw:
+		inventorList.append(nameflipper(inventor))
+	if len(inventorList) == 1:
+		return inventorList[0]
 	else:
-		return ', '.join(inventorlist) 
+		return ', '.join(inventorList) 
 
 
 
@@ -51,39 +44,59 @@ def form_view(request_iter):
 def puller(pn):
 	patentDict = {'number' : pn}
 	# scrape everything but assignee
-	url = 'http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO1&Sect2=HITOFF&d=PALL&p=1&u=%2Fnetahtml%2FPTO%2Fsrchnum.htm&r=1&f=G&l=50&s1={}.PN.&OS=PN/{}&RS=PN/{}'.format(pn,pn,pn)
+	url = 'https://developer.uspto.gov/ibd-api/v1/application/grants?patentNumber={}'.format(pn)
 
-	request_header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
-	page = requests.get(url, headers=request_header).text
-
-	soup = BeautifulSoup(page, 'html.parser')
+	result = requests.get(url, headers={'accept': 'application/json'}).json()
 	
-	patentDict['title'] = soup.find("font", {"size": "+1"}).text
+	patentDict['title'] = result["results"][0]["inventionTitle"]
+	patentDict['abstract'] = result["results"][0]["abstractText"][0]
 	
-	inventorcolumn = soup.find("th").parent.td
-	inventorsraw = inventorcolumn.find_all('b')
-	patentDict['inventors'] = namelister(inventorsraw)
-	patentDict['abstract'] = soup.find('p').text
+	inventorsRaw = result["results"][0]["inventorNameArrayText"]
+	patentDict['inventors'] = namelister(inventorsRaw)
+	
+	originalAssignee = result["results"][0]["assigneeEntityName"]
+	
 
-	# pull assignment information
+	# pull reassignment information
 	url = 'https://assignment-api.uspto.gov/patent/lookup?query={}&filter=PatentNumber&fields=main'.format(pn)
-	tree = ET.fromstring(requests.get(url).text)
+	rawXML = requests.get(url).text
+	
+	# Check if firm worked on patent previously
+	firmName = 'NameOfFirm'
+	firmNameFoundIn = ''
+	if firmName.upper() in rawXML:
+		firmNameFoundIn = str(pn)
+	
+	tree = ET.fromstring(rawXML)
+	presumptiveAssignee = tree.findtext(".//*[str='ASSIGNMENT OF ASSIGNORS INTEREST (SEE DOCUMENT FOR DETAILS).']/*[@name='patAssigneeName']/str")
 	# finds first patAssigneeName that's a sibling of an assignment of 
 	# assignor's interest. This skips security interest assignments.
-	assignee = tree.findtext(".//*[str='ASSIGNMENT OF ASSIGNORS INTEREST (SEE DOCUMENT FOR DETAILS).']/*[@name='patAssigneeName']/str")
-	# Checks to see if any assignee changed its name
-	nameChangeAssignee = tree.findtext(".//*[str='CHANGE OF NAME (SEE DOCUMENT FOR DETAILS).']/*[@name='patAssigneeName']/str")
-	nameChangeAssignor = tree.findtext(".//*[str='CHANGE OF NAME (SEE DOCUMENT FOR DETAILS).']/*[@name='patAssignorName']/str")
-	if nameChangeAssignee is not None:
-		if nameChangeAssignor.split(' ')[0] == assignee.split(' ')[0]:
-		# A previous assignee may have changed its name and then reassigned 
-		# so this checks to make sure the name change is for the presumptive
-		# assignee. Sometimes there are slight discrepancies in the name   
-		# so it compares only the first word. Still doesn't catch this edge case:
-		# Initial Inc. renamed Initial LLC reassigns to Final Inc.
-			assignee = nameChangeAssignee
-	patentDict['assignee'] = assignee.title().replace('Llc','LLC')
-	return patentDict
+	if presumptiveAssignee is not None:
+	# checks to see that the patent has been reassigned at least once
+		nameChangeAssignee = tree.findtext(".//*[str='CHANGE OF NAME (SEE DOCUMENT FOR DETAILS).']/*[@name='patAssigneeName']/str")
+		nameChangeAssignor = tree.findtext(".//*[str='CHANGE OF NAME (SEE DOCUMENT FOR DETAILS).']/*[@name='patAssignorName']/str")
+		if nameChangeAssignee is not None:
+		# Checks to see if any assignee changed its name
+			if nameChangeAssignor.split(' ')[0] == presumptiveAssignee.split(' ')[0]:
+			# A previous assignee may have changed its name and then reassigned 
+			# so this checks to make sure the name change is for the presumptive
+			# assignee. Sometimes there are slight discrepancies in the name   
+			# so it compares only the first word. Still doesn't catch this edge case:
+			# Initial Inc. renamed Initial LLC reassigns to Final Inc.
+				finalAssignee = nameChangeAssignee
+			else:
+				# somebody changed names but it wasn't the presumptive assignee
+				finalAssignee = presumptiveAssignee
+		else:
+			# nobody changed names
+			finalAssignee = presumptiveAssignee
+	else: # patent never reassigned; belongs to original applicant
+		if originalAssignee:
+			finalAssignee = originalAssignee
+		else:
+			finalAssignee =  '(original)'		# placeholder
+	patentDict['assignee'] = finalAssignee.title().replace('Llc','LLC')
+	return [patentDict, firmNameFoundIn]
 def patent_form(request):
 
 	# If this is a POST request then process the Form data
@@ -96,16 +109,19 @@ def patent_form(request):
 		if form.is_valid():
 			
 			patentDictList = []
+			firmNameFoundList = []
 			
 			patentNumberList = form.cleaned_data['patents']
 			
 			with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
 				future_to_patent = {executor.submit(puller, pn): pn for pn in patentNumberList}
 				for future in concurrent.futures.as_completed(future_to_patent):
-					patentDictList.append(future.result())
+					patentDictList.append(future.result()[0])
+					firmNameFoundList.append(future.result()[1])
 
 			context = {
 				'patentDictList' : patentDictList,
+				'firmNameFoundList': firmNameFoundList,
 			}
 			
 			return render(request, 'puller/patent_results.html', context)
